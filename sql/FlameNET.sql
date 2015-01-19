37b457d7a0ed64eddf2f5b9a1e96b429bfa162ec
One <projectonecore@gmail.com>
2015-01-18 19:22:16 -0600

    [38] Web/DB: add sql account

--- sql/FlameNET.sql
+++ sql/FlameNET.sql
@@ -13,6 +13,30 @@ MySQL - 5.6.21 : Database - FlameNET
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+/*Table structure for table `account` */
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `Id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `fist_name` varchar(35) NOT NULL DEFAULT '',
+  `last_name` varchar(35) NOT NULL DEFAULT '',
+  `mail` varchar(25) NOT NULL DEFAULT '' COMMENT 'example@example.com',
+  `password` varchar(65) NOT NULL DEFAULT '',
+  `secret_question` varchar(35) NOT NULL DEFAULT '',
+  `answer_question` varchar(35) NOT NULL DEFAULT '',
+  `country` varchar(15) NOT NULL DEFAULT '',
+  `date_of_birth` varchar(25) NOT NULL DEFAULT '',
+  `roles_account` varchar(1) NOT NULL DEFAULT '2',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `account` */
+
+LOCK TABLES `account` WRITE;
+
+UNLOCK TABLES;
+
 /*Table structure for table `cms_ver` */
 
 DROP TABLE IF EXISTS `cms_ver`;
