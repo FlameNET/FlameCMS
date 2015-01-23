b2d6d26d30f91068d749de0e0b4ecca2df979c82
One <projectonecore@gmail.com>
2015-01-23 12:39:06 -0600

    [47] Web/Error: add function setReporting

--- system/functions/function.php
+++ system/functions/function.php
@@ -120,3 +120,16 @@ function Year($year){
 
 	return $year;
 }
+
+function setReporting() {
+	if (DEVELOPMENT_ENVIRONMENT == true) {
+		error_reporting(E_ALL);
+		ini_set('display_errors','On');
+	} else {
+		error_reporting(E_ALL);
+		ini_set('display_errors','Off');
+		ini_set('log_errors', 'On');
+		ini_set('error_log', ROOT.DS.'system'.DS.'tmp'.DS.'logs'.DS.'error.log');
+	}
+}
+setReporting();
