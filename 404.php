277a108df7d63542d2094a060282cdfc8ac741e0
Alexandros Papadopoulos <alex_pap_2010@live.com>
2015-02-11 14:24:03 +0200

    [70] New 404 Page
    Finished the 404 and removed the f-ugly one.
    I hope you will like the templates i work on :+1:

--- 404.php
+++ 404.php
@@ -1,25 +1,41 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN">
-<html>
-
-	<head>
-		<title>404</title>
-	</head>	<body>
-<body bgcolor="Black">
-<center><img src="assets/images/404/404.jpg" /> </center>
-
-
-<center><img src="assets/images/404/4042.PNG" /> </center>
-<center><h2 style="font-family:Garamond, Georgia; color:white; font-size:20px;">What did you? You punny little peasant! You broke it!</center>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<center><h2 style="font-family:Garamond, Georgia; color:white; font-size:12px;">All Images Copyright Blizzard Entertainment 2010</center>
-	</body>
+<!-- DO NOT TOUCH -->
+<?php require_once('system/config.php'); ?>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>404 - <?php echo TITLE ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok3v=1613a3a185/"},atok:"1241d2d2c55155c724d4e2f1ed900b60",petok:"06fd1f55f9fa416f3b7ee9946db03423ad337489-1423654506-1800",zone:"tonybogdanov.com",rocket:"0",apps:{}}];CloudFlare.push({"apps":{"ape":"88830b01fac5ac18aab0f099f034c785"}});!function(a,b){a=document.createElement("script"),b=document.getElementsByTagName("script")[0],a.async=!0,a.src="<?php echo BASE_URL ?>assets/js/404/cloudflare.min.js",b.parentNode.insertBefore(a,b)}()}}catch(e){};
+//]]>
+</script>
+<link href="http://preview.tonybogdanov.com/project/creatica-error/css/style.css" rel="stylesheet">    
+<script src="<?php echo BASE_URL ?>assets/js/404/jquery.min.js"></script>    
+<link rel="shortcut icon" href="<?php echo BASE_URL ?>assets/images/logos/favicon.png" />
+</head>
+<body class="background-navy">
+<header>
+<div class="logo"><a href="<?php echo BASE_URL ?>"><img src="<?php echo BASE_URL ?>assets/images/404/logo.png" class="logo" alt="" /></a></div>
+<div class="ribbon"><img src="<?php echo BASE_URL ?>assets/images/404/ribbon.png" alt="" /></div>
+</header>
+        
+<section data-error="404" class="error">
+<div class="number">
+<div id="n1"></div>
+<div id="n2"></div>
+<div id="n3"></div>
+</div>
+</section>
+<footer>
+<div class="container">
+<ul class="social">
+<li><a href="<?php echo BASE_URL ?>"><img src="<?php echo BASE_URL ?>assets/images/404/home-relink.png" alt="" /></a></li>
+<li><a href="<?php echo BASE_URL ?>shop/"><img src="<?php echo BASE_URL ?>assets/images/404/shop-relink.png" alt="" /></a></li>
+<li><a href="https://www.facebook.com/pages/WoWFailureCMS-AquaflameCMS/141791002519526?ref=hl"><img src="<?php echo BASE_URL ?>assets/images/404/fb-relink.png" alt="" /></a></li>
+</ul>
+</div>
+</footer>
+<script src="http://preview.tonybogdanov.com/project/creatica-error/js/style.js"></script>
+</body>
 </html>
