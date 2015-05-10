fedbdc9559e76aef1cbbd9201cd5e6ba5214a7ad
One <projectonecore@gmail.com>
2015-05-10 12:06:41 -0600

    Class/News: fix (Bug) Ago Method... now works correctly

--- system/library/news.class.php
+++ system/library/news.class.php
@@ -118,25 +118,54 @@ class News {
 		return $truncate;
 	}
 
-	public function ago($time) {
-		$periods	= array("second", "minute", "hour", "day", "week", "month", "year", "decade");
-		$lengths	= array("60","60","24","7","4.35","12","10");
-		$now		= time();
-		$difference	= $now - $time;
-		$tense		= "ago";
+	public function AgoTimeFlame($date) {
 
-		for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+		if(empty($date)){
+			return "No date provided";
+		}
+
+		$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+		$lengths = array("60","60","24","7","4.35","12","10");
+		$now = time();
+
+		$unix_date = strtotime( $date );
+
+		/**
+		 *  check validity of date
+		 */
+
+		if( empty( $unix_date ) )
+		{
+			return "Bad date";
+		}
+
+		/**
+		 *  Is it future date or past date
+		 */
+
+		if( $now > $unix_date )
+		{
+			$difference = $now - $unix_date;
+			$tense = "ago";
+		}
+		else
+		{
+			$difference = $unix_date - $now;
+			$tense = "from now";
+		}
+
+		for( $j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++ )
+		{
 			$difference /= $lengths[$j];
 		}
 
-		$difference = round($difference);
+		$difference = round( $difference );
 
-		if($difference != 1) {
+		if( $difference != 1 )
+		{
 			$periods[$j].= "s";
 		}
 
-		return "$difference $periods[$j] ago";
-
+		return "$difference $periods[$j] {$tense}";
 	}
-
 }
